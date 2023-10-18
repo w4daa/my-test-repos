530bafd5fae6b7eb123dfaafc4cddba9a40ab818
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2023-10-18 22:29:22 +0200

    simulation of code sharing 3

--- src/main/java/com/project/schoolmanagment/SchoolManagementApplication.java
+++ src/main/java/com/project/schoolmanagment/SchoolManagementApplication.java
@@ -1,17 +1,14 @@
 package com.project.schoolmanagment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
 
 @SpringBootApplication
-@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class SchoolManagementApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(SchoolManagementApplication.class, args);
-        //****---------
-        //*****-------
-        //*****-------
+        //****--------
     }
 
 }
